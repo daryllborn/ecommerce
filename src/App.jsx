dea4cfe2195371ad36c07848eb3a58fd2cea4cd2
Daryll Born <born.dd@gmail.com>
2021-10-13 14:50:37 +0200

    Added React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 import Home from "./pages/Home";
-//import ProductList from "./pages/ProductList";
-//import Product from "./pages/Product";
-//import Register from "./pages/Register";
-//import Login from "./pages/Login";
-//import Cart from "./pages/Cart";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 
 const App = () => {
-  return <Home />
+  return (
+  
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </Switch>
+    </Router>
+  )
 };
 
 export default App;
